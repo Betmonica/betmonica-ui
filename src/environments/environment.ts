ffa092f31e6114c58464e2d7e77789a2a6840146
VadimParpolita <vadim.parpolita@theraven.tech>
2023-09-02 18:48:33 +0300

    firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from '../app/core/interfaces';
 
 export const environment: Environment = {
-  production: false,
-  apiUrl: 'http://localhost:10000/api'
+	production: true,
+	apiUrl: 'https://betmonica.onrender.com/api'
 };
